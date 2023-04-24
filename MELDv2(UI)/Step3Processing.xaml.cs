f6835ec3dbd9923e4f63f8d3f0c4de79e806235d
muridse <mur1dse@mail.ru>
2023-04-24 15:45:34 +0300

    UI done

--- MELDv2(UI)/Step3Processing.xaml.cs
+++ MELDv2(UI)/Step3Processing.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,7 +26,38 @@ namespace MELDv2_UI_
             InitializeComponent();
             doneButton.Click += Exit;
             backButton.Click += OpenBack;
+            submitButton.Click += StartConvert;
+            openFolder.Click += OpenExplorer;
 
+        }
+
+        private void OpenExplorer(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer.exe", ConfigRepository.Config.MeltConfiguration.FolderPath);
+        }
+
+        private void StartConvert(object sender, RoutedEventArgs e)
+        {
+            progressBar.Visibility = Visibility.Visible;
+            progressBar.Value = 5;
+           var status = new MELDv2.MeldProcessor(ConfigRepository.Config.MeltConfiguration).Run();
+            if (status == -1)
+            {
+                statusText.Background = new SolidColorBrush(Colors.Red);
+                statusText.Text = "Завершено с ошибкой!";
+                progressBar.Value = 0;
+            }
+            else 
+            {
+                statusText.Text = String.Format($"Выгружено {status} тегов!");
+                openFolder.Visibility = Visibility.Visible;
+                for (int i = (int)progressBar.Value; i <= 100; i++)
+                {
+                    progressBar.Value++;
+                    Thread.SpinWait(100);
+                }
+            }
+            
         }
 
         private void Exit(object sender, RoutedEventArgs e)
